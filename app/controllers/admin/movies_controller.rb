a9a6228a0421df55543912acddb53091dc2e034a
root <@satt.jp>
2023-12-12 12:06:10 +0000

    station03

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -3,5 +3,29 @@ module Admin
     def index
       @movies = Movie.all
     end
+
+    def new
+      @movie = Movie.new()
+    end
+
+    def create
+      @movie = Movie.new(movie_params)
+      if @movie.valid?
+        begin
+          @movie.save!
+          redirect_to admin_movies_path
+        rescue ActiveRecord::StatementInvalid => e
+          flash[:error] = 'DBエラーです'
+          render :new
+        end
+      else
+        render :new
+      end
+    end
+
+    private
+    def movie_params
+      params.require(:movie).permit(:name, :year, :is_showing, :description, :image_url)
+    end
   end
 end
