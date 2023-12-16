8fce6325fa94ee46f8ed3ed4f8af01ed12f9cc13
root <@satt.jp>
2023-12-16 04:10:32 +0000

    station06

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,24 @@
 class MoviesController < ApplicationController
 
   def index
-    @movies = Movie.all
+
+    if movie_search_params[:keyword].blank? && movie_search_params[:is_showing].blank?
+      @movies = Movie.all
+    end
+
+    if movie_search_params[:keyword].present? && movie_search_params[:is_showing].present?
+      @movies = Movie.where('name LIKE :keyword OR description LIKE :keyword', keyword: "%#{movie_search_params[:keyword]}%")
+        .and Movie.where(is_showing: movie_search_params[:is_showing])
+    elsif movie_search_params[:is_showing].present?
+      @movies = Movie.where(is_showing: movie_search_params[:is_showing])
+    else
+      @movies = Movie.where('name LIKE :keyword OR description LIKE :keyword', keyword: "%#{movie_search_params[:keyword]}%")
+    end
+  end
+
+  private
+  def movie_search_params
+    params.permit(:keyword, :is_showing)
   end
 
 end
